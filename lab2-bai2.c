7a32d24b94749de6c9b1eaa512f289bd5525d116
huytv2211 <huytv22@fpt.edu.vn>
2025-09-08 09:31:12 +0700

    Hoàn thành Lab2

---
+++ lab2-bai2.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+int main() {
+    // int a, b;
+    // printf("Nhap a: ");
+    // scanf("%d", &a);
+    // printf("Nhap b: ");
+    // scanf("%d", &b);
+    // int sum = 0;
+    // int n = a+ b;
+    // printf("Tong cua a va b la: %d\n", n);
+    // bài 1 lab 2
+      // int a, b;
+    printf("Nhap a: ");
+    scanf("%d", &a);
+    printf("Nhap b: ");
+    scanf("%d", &b);
+    int sum = 0;
+    int n = a+ b;
+    printf("Tong cua a va b la: %d\n", n);
+    // bài 2 lab 2
+
+    // bào 3 lab 2
+
+
+    return 0;
+}
